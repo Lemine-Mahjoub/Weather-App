fed0f5f4ff7c6acf8b04fe57b2b64b49cade9705
Lemine-Mahjoub <leminemahjoub@gmail.com>
2024-11-13 02:46:28 +0100

    :sparkles: Api Key and env file :sparkles:

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+
+const location = 'saint-jeannet';
+
 export default function App() {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${process.env.URL}?key=${process.env.API_KEY}&q=${location}`)
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text onPress={fetchData}>Press to fetch data!</Text>
       <StatusBar style="auto" />
     </View>
   );
