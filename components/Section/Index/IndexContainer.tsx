763467cdc2bf34280ff2ba160c80d82fd52ebade
Lemine-Mahjoub <leminemahjoub@gmail.com>
2024-11-20 11:23:27 +0100

    :bug: Working Build IOS Android :bug:

--- components/Section/Index/IndexContainer.tsx
+++ components/Section/Index/IndexContainer.tsx
@@ -3,9 +3,7 @@ import { View, StyleSheet, Text } from 'react-native';
 import { useGetWeather } from '@/hooks/useGetWeather';
 import { useGetCityName } from '@/hooks/useGetCityName';
 export const IndexContainer: React.FC = () => {
-  const city = useGetCityName();
-  console.log(city);
-  const { weatherData, loading, error } = useGetWeather(city ?? 'Paris');
+  const { weatherData, loading, error } = useGetWeather('Paris');
 
   return (
     <View style={styles.container}>
@@ -22,6 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
   },
 });
