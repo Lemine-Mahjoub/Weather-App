1bc2375d877e07e860b731b852892b0aadc036a9
Lemine-Mahjoub <leminemahjoub@gmail.com>
2024-11-20 02:42:02 +0100

    :bug: Remove useless dist Files :bug:

--- app/(tabs)/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Index_1 = require("@/components/Section/Index");
-function HomeScreen() {
-    return (React.createElement(Index_1.IndexContainer, null));
-}
-exports["default"] = HomeScreen;
